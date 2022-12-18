a8101bed27b874935cdf34b28ce0a3632b01e5a2
ha-zu <nagaoka0132@gmail.com>
2022-12-19 05:35:03 +0900

    Modified voice samples.

--- posture-correction-app/config/constant_list.py
+++ posture-correction-app/config/constant_list.py
@@ -11,15 +11,15 @@ VOICE_VOLUME = 0.7
 Instructions correct posture
 """
 # squat
-FORWORD_POSTURE = '姿勢を前に倒してください'
-BACKWORD_POSTURE = '姿勢を後ろに倒してください'
-FORWORD_KNEE = '膝をつま先より後ろにしてください'
-BUTTOCKS_UP = 'お尻を上げてください'
-BUTTOCKS_DOWN = 'お尻を下げてください'
+FORWORD_POSTURE = "姿勢を前に倒してください"
+BACKWORD_POSTURE = "姿勢を後ろに倒してください"
+FORWORD_KNEE = "膝をつま先より後ろにしてください"
+BUTTOCKS_UP = "お尻を上げてください"
+BUTTOCKS_DOWN = "お尻を下げてください"
 
 # plank
-UP_HEAD = '顔を上げてください'
-UP_WAIST = '腰を上げてください'
-DOWN_WAIST = '腰を下げてください'
-UP_CHEST = '胸を上げてください'
-DOWN_CHEST = '胸を下げてください'
+UP_HEAD = "顔を上げてください"
+UP_WAIST = "腰を上げてください"
+DOWN_WAIST = "腰を下げてください"
+UP_CHEST = "胸を上げてください"
+DOWN_CHEST = "胸を下げてください"
